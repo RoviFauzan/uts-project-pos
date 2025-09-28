591bd4e4ac7091ccd2d501bebf153c7e0e29783c
RoviFauzan <rovifauzan24@gmail.com>
2025-09-28 18:16:17 +0700

    Update database connection settings for Docker

--- website/App/controller/Database.php
+++ website/App/controller/Database.php
@@ -1,13 +1,15 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$database="aplikasi_kasir";
+// Pengaturan koneksi untuk lingkungan Docker
+$host     = "db";
+$user     = "crud_user";
+$pass     = "12345678"; // Pastikan ini sama dengan di docker-compose.yml
+$database = "crud_db";
 
-$conn = new mysqli($host,$user,$pass,$database);
+// Membuat koneksi ke database
+$conn = new mysqli($host, $user, $pass, $database);
 
+// Memeriksa apakah koneksi berhasil atau gagal
 if (mysqli_connect_errno()){
-		trigger_error("Koneksi gagal : ". mysqli_connect_error(), E_USER_ERROR);
-		
-		}
+    trigger_error("Koneksi gagal : ". mysqli_connect_error(), E_USER_ERROR);
+}
 ?>
